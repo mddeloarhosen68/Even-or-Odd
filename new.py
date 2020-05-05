c355ba611560c2e906460d5802449f41d417352f
mddeloarhosen68 <62032396+mddeloarhosen68@users.noreply.github.com>
2020-05-06 02:10:39 +0600

    Add files via upload

---
+++ new.py
@@ -0,0 +1,37 @@
+#Ex:1
+x = 7
+r = x % 2
+
+if r == 0:
+    print("Even")
+
+else:
+    print("Odd")
+
+#Ex:2
+x = 7
+r = x % 2
+
+if r == 0:
+    print("Even")
+    if x > 5:
+        print("Great")
+    else:
+        print("Not so Great")
+
+else:
+    print("Odd")
+
+#Ex:3
+x = 4
+
+if x == 1:
+    print("One")
+elif x == 2:
+    print("Two")
+elif x == 3:
+    print("Three")
+elif x == 4:
+    print("Four")
+else:
+    print("This is Wrong input")
